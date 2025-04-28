f066b43087361036507f5cef158bf1918d653985
J.J. Allaire <jj.allaire@gmail.com>
2025-04-28 15:53:16 -0400

    load with arquero

--- js/shared_df.tsx
+++ js/shared_df.tsx
@@ -1,18 +1,20 @@
 import * as React from "react";
 import { createRender, useModelState } from "@anywidget/react";
 
+import * as aq from 'arquero';
+
 const render = createRender(() => {
 
 
 	const [id] = useModelState<string>("_id");
 	console.log(id)
 
-	const [df] = useModelState<DataView>("_df_bytes");
+	const [df_bytes] = useModelState<DataView>("_df_bytes");
+
+	const dt = aq.fromArrow(new Uint8Array(df_bytes.buffer, df_bytes.byteOffset, df_bytes.byteLength));
 
 
-
-
-	console.log(df)
+	console.log(dt)
 
 	return (
 		<div className="shared_df">
