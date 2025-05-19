f63980add3174be78687d40b5695e64d36ed3208
J.J. Allaire <jj.allaire@gmail.com>
2025-05-19 16:24:17 -0400

    duckdb info log level

--- js/coordinator/duckdb.ts
+++ js/coordinator/duckdb.ts
@@ -21,7 +21,7 @@ export async function initDuckdb() {
 
     // Instantiate the asynchronous version of DuckDB-wasm
     const worker = new Worker(worker_url);
-    const logger = new ConsoleLogger(LogLevel.WARNING);
+    const logger = new ConsoleLogger(LogLevel.INFO);
     const db = new AsyncDuckDB(logger, worker);
     await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
     URL.revokeObjectURL(worker_url);
