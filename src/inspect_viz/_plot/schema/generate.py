0557bc367a702ec76677aff61890849fa2da1b0c
J.J. Allaire <jj.allaire@gmail.com>
2025-05-21 08:04:21 -0400

    generate plot

--- src/inspect_viz/_plot/schema/generate.py
+++ src/inspect_viz/_plot/schema/generate.py
@@ -1,11 +1,18 @@
 import subprocess
 from pathlib import Path
 
+# mypy: disable-error-code="type-arg"
+# ruff: noqa: D200, D301, W605, D205
+
+# from builtins import float as float_aliased
+
+# re-order spec / hconcat / remove not plot types
+
 
 def generate_mosaic_models() -> None:
     dir = Path(__file__).parent
     schema = (dir / "v0.16.2.json").as_posix()
-    models = (dir / "mosaic.py").as_posix()
+    models = (dir / "plot.py").as_posix()
     subprocess.run(
         [
             "datamodel-codegen",
@@ -20,6 +27,7 @@ def generate_mosaic_models() -> None:
             "--target-python-version",
             "3.10",
             "--use-schema-description",
+            "--use-standard-collections",
         ]
     )
 
