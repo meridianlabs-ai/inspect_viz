b72587d276ba56294f56c486c37276402362b3f7
J.J. Allaire <jj.allaire@gmail.com>
2025-05-25 11:11:57 -0400

    export layout functions

--- src/inspect_viz/__init__.py
+++ src/inspect_viz/__init__.py
@@ -1,4 +1,5 @@
 from ._core import Data, Param, Selection, Widget
+from ._layout import hconcat, hspace, vconcat, vspace
 from ._plot import line_y
 
 __all__ = [
@@ -6,5 +7,9 @@ __all__ = [
     "Param",
     "Selection",
     "Widget",
+    "hconcat",
+    "vconcat",
+    "hspace",
+    "vspace",
     "line_y",
 ]
---
+++ src/inspect_viz/_layout/__init__.py
@@ -0,0 +1,4 @@
+from .concat import hconcat, vconcat
+from .space import hspace, vspace
+
+__all__ = ["hconcat", "vconcat", "hspace", "vspace"]
