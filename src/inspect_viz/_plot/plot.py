2745ad00bcc926461036a461db9b9c68939e2c79
J.J. Allaire <jj.allaire@gmail.com>
2025-05-25 14:07:40 -0400

    plot w/ marks

--- src/inspect_viz/_plot/plot.py
+++ src/inspect_viz/_plot/plot.py
@@ -1,7 +1,35 @@
-from inspect_viz._core import Data, Widget
-from inspect_viz.mosaic import Plot
+from typing import Any, Literal
+
+from shortuuid import uuid
+
+from inspect_viz._core import Widget
+from inspect_viz.mosaic import HConcat, Legend, Plot
 
 
-def plot(*mark: Widget) -> Widget:
+def plot(
+    *mark: Widget,
+    grid: bool | str = False,
+    x_label: str | None = None,
+    y_label: str | None = None,
+    legend: Literal["color", "opacity", "symbol"] | None = None,
+) -> Widget:
+    """Plot.
+
+    Args:
+        *mark: Plot marks.
+        grid: Whether to show a grid aligned with the scale’s ticks. If true, show a grid with the current color stroke; if a string, show a grid with the specified stroke color;
+        x_label: A textual label to show on the axis or legend; if `None`, show no label.
+        y_label: A textual label to show on the axis or legend; if `None`, show no label.
+        legend: foo
+    """
+    # create plot
     components = [m.component for m in mark]
-    return Widget(component=Plot(plot=components))  # type: ignore[arg-type]
+    plot = Plot(plot=components, grid=grid, xLabel=x_label, yLabel=y_label)  # type: ignore[arg-type]
+
+    # wrap with legend if specified
+    if legend is not None:
+        plot.name = uuid()
+        legend_args: dict[str, Any] = dict(legend=legend, for_=plot.name, columns=1)
+        return Widget(HConcat(hconcat=[plot, Legend(**legend_args)]))
+    else:
+        return Widget(plot)
