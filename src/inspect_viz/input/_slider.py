bc04f3c4d9b45f4912b8966e74f77f8bd23591cf
J.J. Allaire <jj.allaire@gmail.com>
2025-06-10 12:32:29 -0700

    add select to slider

--- src/inspect_viz/input/_slider.py
+++ src/inspect_viz/input/_slider.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Literal
 
 from inspect_viz._util.marshall import dict_remove_none
 
@@ -8,11 +8,12 @@ from .._core import Component, Data, Param, Selection
 def slider(
     data: Data | None = None,
     *,
+    filter_by: str | None = None,
     label: str | None = None,
     column: str | None = None,
     field: str | None = None,
-    filter_by: str | None = None,
     selection: Selection | None = None,
+    select: Literal["point", "interval"] | None = None,
     param: Param | None = None,
     value: float | None = None,
     min: float | None = None,
@@ -24,12 +25,13 @@ def slider(
 
     Args:
        data: The data source for this widget. Used in conjunction with the `column` property. The minimum and maximum values of the column determine the slider range.
+       filter_by: A selection to filter the data source indicated by the `data` property.
        label: A text label for this input (optional).
        column: The name of a database column whose values determine the slider range. Used in conjunction with the `data` property. The minimum and maximum values of the column determine the slider range.
        field: The database column name to use within generated selection clause predicates. Defaults to the `column` property.
-       filter_by: A selection to filter the data source indicated by the `data` property.
        selection: A selection to target with the selected `column` or `field` (defaults to the data source selection).
-       param: A parameter to set with the current slider value (if `param` is specified then `field` and `selection` parameters are not used).
+       select: The type of selection clause predicate to generate when `selection` is specified. If `'point'` (the default), the selection predicate is an equality check for the slider value. If `'interval'`, the predicate checks an interval from the minimum to the current slider value.
+       param: A parameter to set with the current slider value (if `param` is specified then `field`, `selection`, and `select` are ignored.
        value: The initial slider value.
        min: The minumum slider value.
        max: The maximum slider value.
@@ -39,6 +41,7 @@ def slider(
     config: dict[str, Any] = dict_remove_none(
         {
             "input": "slider",
+            "select": select,
             "value": value,
             "min": min,
             "max": max,
