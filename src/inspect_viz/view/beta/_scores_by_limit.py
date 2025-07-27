72d15b3035be4c751d048e63ede0b6e5b3bfaac1
hcoppockno10 <Harry.Coppock@dsit.gov.uk>
2025-07-27 12:17:12 +0000

    adding limit function

---
+++ src/inspect_viz/view/beta/_scores_by_limit.py
@@ -0,0 +1,111 @@
+from typing import Unpack
+
+import numpy as np
+import pandas as pd
+
+from inspect_viz import Component, Data, Selection
+from inspect_viz._core.param import Param
+from inspect_viz._util.channels import resolve_log_viewer_channel
+from inspect_viz._util.notgiven import NOT_GIVEN, NotGiven
+from inspect_viz._util.stats import z_score
+from inspect_viz.interactor import highlight, nearest_x, nearest_y, region, toggle
+from inspect_viz.mark import area_y, bar_y, dot, line, line_x, rule_x
+from inspect_viz.plot import legend, plot
+from inspect_viz.plot._attributes import PlotAttributes
+from inspect_viz.transform import sql
+
+
+def scores_by_limit(
+    data: Data,
+    x: str = "token_limit",
+    y: str = "success_rate",
+    y_stderr: str | None = "standard_error",
+    fx: str | None = "difficulty",
+    color_by: str | None = "model_id",
+    other_termination_rate: str | None = "other_termination_rate",
+    y_ci: bool | float = 0.95,
+    y_label: str | None | NotGiven = NOT_GIVEN,
+    width: float | Param | None = None,
+    height: float | Param | None = None,
+    **attributes: Unpack[PlotAttributes],
+) -> Component:
+    """Line plot for investigating success rate as a function of compute budget.
+
+    Here, model success rate is plotted as a function of the compute budget whether that be cost, token usage, time, number of messages, etc.
+
+    Args:
+       data: Evals data table. This is typically created using a data frame read with the inspect `evals_df()` function.
+       x: Name of field for x axis (defaults to "limit" - the theoretical token limit).
+       y: Name of field for y axis (defaults to "success_rate").
+       y_stderr: Name of field for stderr (defaults to "standard_error").
+       fx: Name of field for x facet (defaults to "difficulty")
+       color_by: Name of field to color by (defaults to "model_id").
+       other_termination_rate: Name of the field which corresponds to the proportion of samples which terminated for a diferent reason to that specified by limit
+       y_ci: Confidence interval (e.g. 0.80, 0.90, 0.95, etc.). Defaults to 0.95.
+       y_label: Y axis label (pass None for no label).
+       width: The outer width of the plot in pixels, including margins. Defaults to 700.
+       height: The outer height of the plot in pixels, including margins. The default is width / 1.618 (the [golden ratio](https://en.wikipedia.org/wiki/Golden_ratio))
+       **attributes: Additional `PlotAttributes`. By default, the `y_inset_top` and `margin_bottom` are set to 10 pixels and `x_ticks` is set to `[]`.
+    """
+
+    # establish channels
+    channels: dict[str, str] = {}
+    if fx == "difficulty":
+        channels["Difficulty"] = fx
+    if x == "token_limit":
+        channels["Token Limit"] = x
+    if y == "success_rate":
+        channels["Success Rate"] = y
+    if color_by == "model_id":
+        channels["Model"] = color_by
+
+    resolve_log_viewer_channel(data, channels)
+    # allow user to select model to highlight
+    model = Selection.single()
+
+    z_alpha = z_score(y_ci) if isinstance(y_ci, (int, float)) else 0.95
+
+    components = [
+        line(data, x=x, stroke=color_by, y=y, fx=fx, tip=True, channels=channels),
+    ]
+
+    if y_stderr:
+        components.append(
+            area_y(
+                data,
+                x=x,
+                y=y,
+                y1=sql(f"{y} - ({z_alpha} * {y_stderr})"),
+                y2=sql(f"{y} + ({z_alpha} * {y_stderr})"),
+                color=color_by,
+                fill=color_by,
+                fill_opacity=0.3,
+                fx=fx,
+                channels=channels,
+            )
+        )
+
+    components.extend(
+        [
+            nearest_x(target=model, channels=["color"]),
+            highlight(by=model, opacity=0.2, fill_opacity=0.1),
+        ]
+    )
+
+    # resolve defaults
+    defaults: PlotAttributes = {
+        "x_scale": "log",
+    }
+    attributes = defaults | attributes
+
+    # render plot
+    return plot(
+        components,
+        x_label=x,
+        fx_label=None,
+        y_label=y_label,
+        width=width,
+        height=height,
+        legend=legend("color", location="bottom"),
+        **attributes,
+    )
