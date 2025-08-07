16f67a4986d52ccfb76a570e273fd0597ffcf46f
Charles Teague <charles@merdianlabs.ai>
2025-08-06 20:46:44 -0400

    Disable Table, Legend Schema validation
    Both tests are comparing to the mosaic schema, but we have made changes.
    
    Table:
    - columns argument has differing types
    - other args not set
    
    Legend:
    - additional options passed to be consumed by component

--- tests/mosaic/test_input_wrappers.py
+++ tests/mosaic/test_input_wrappers.py
@@ -1,7 +1,8 @@
+from pytest import mark
 from inspect_viz import Data
 from inspect_viz._core.selection import Selection
 from inspect_viz.input import search, slider
-from inspect_viz.table import table
+from inspect_viz.table import column, table
 
 from ._schema import Search, Slider, Table
 from .utils import check_component
@@ -30,14 +31,13 @@ def test_slider_wrapper(penguins: Data) -> None:
     )
 
 
+@mark.xfail(reason="Table schema no longer matches mosaic component")
 def test_table_wrapper(penguins: Data) -> None:
     check_component(
         table(
             penguins,
             filter_by=Selection("intersect"),
-            align={"bill_length": "left"},
-            columns=["bill_length", "bill_depth"],
-            width={"bill_length": 10},
+            columns=[column("bill_length", align="left", width=10), "bill_depth"],
             max_width=22,
             height=200,
         ),
--- tests/mosaic/test_plot_wrappers.py
+++ tests/mosaic/test_plot_wrappers.py
@@ -1,3 +1,4 @@
+from pytest import mark
 from inspect_viz import Component, Data
 from inspect_viz.plot import legend, plot
 
@@ -16,6 +17,7 @@ def test_plot_wrapper(dot_mark: Component) -> None:
     )
 
 
+@mark.xfail(reason="Legend schema no longer matches mosaic component")
 def test_legend_wrapper(penguins: Data) -> None:
     check_component(
         legend(
